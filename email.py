78d3146d47d075a85bf75690d9a550d8c96f8396
felipe.flores <felipeflores6@gmail.com>
2025-05-15 18:45:24 -0300

    Remove dados sensíveis

--- email.py
+++ email.py
@@ -2,24 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 import smtplib
 import json
+from os import environ
 
 # Configurações do email
-remetente = 'felipeflores6@gmail.com'
-senha = 'sbpawzrxjwjlmobn'
-destinatario = 'felipeflores6@gmail.com'
+remetente = environ['remetente']
+senha = environ['remetente']
+destinatario = environ['remetente']
+headers = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36'
+
 
 # Produto e valor máximo desejado
 produto = 'Galaxy S20 fe'
 valor_maximo = 5000
 
-# URL do Google Shopping para a busca do produto
-url = f'https://www.google.com.br/search?q={produto}&tbm=shop'
+
+# URL do Google Shopping e  parâmetros da consulta
+url = f'https://www.google.com.br/search'
+params= {"q": produto, "tbm" : "shop"}
 
 # Faz a requisição da página
-response = requests.get(url)
+response = requests.get("https://www.google.com/search", headers=headers, params=params)
 
 # Faz o parsing do HTML
-soup = BeautifulSoup(response.content, 'html.parser')
+soup = BeautifulSoup(response.text, "lxml")
 
 # Busca os resultados da pesquisa
 results = soup.find_all('div', class_='xcR77')
